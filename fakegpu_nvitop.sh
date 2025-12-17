665c9280cf4c8b2e704bdf6e090abe34618d3447
FanBB2333 <2504082957@qq.com>
2025-12-17 18:24:23 +0800

    fix: but I don't think so

--- fakegpu_nvitop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# FakeGPU nvitop 包装脚本
-# 使用Python API避免TUI模式的段错误
-
-LD_LIBRARY_PATH=./build:$LD_LIBRARY_PATH \
-LD_PRELOAD=./build/libcudart.so.12:./build/libcuda.so.1:./build/libnvidia-ml.so.1:./build/libcublas.so.12 \
-python3 test_nvitop_wrapper.py "$@"
